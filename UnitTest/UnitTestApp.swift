f7b13f2ef66f73400f48cd18374213c2fe0e67f5
ahmed farid <engfaridooo@gmail.com>
2024-09-18 16:46:25 +0300

    Initial Commit

---
+++ UnitTest/UnitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestApp.swift
+//  UnitTest
+//
+//  Created by Farido on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
